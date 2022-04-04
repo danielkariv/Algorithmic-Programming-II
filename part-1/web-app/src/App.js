0d47f5453df49c4c52a8a10b54ac68c2025d0b44
Daniel Kariv <danielkariv98@gmail.com>
2022-04-04 20:44:52 +0300

    Adds comments to describe different logic in code.
    Adds comments and general TODO lines for different parts in code, so it would be more clear what needs to be done, and what each line does.

--- part-1/web-app/src/App.js
+++ part-1/web-app/src/App.js
@@ -7,15 +7,24 @@ import Messenger from './Messenger';
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function App() {
-  
+  // not sure what we should save here. Probably display name, and some kind of a key of user (ID/Email).
+  // other stuff should be saved locally at messenger component.
   var [user, setUser] = useState(null);
   setUser = (name)=>{
     console.log("Trying update user info. : " + name)
   };
+  // not final but I won't put too much thinking about it, we will drop it when we will have a backend server.
+  // So, it needs only the basic information needed for messanger, and to login/register.
+  var [users,setUsers] = useState([
+    {email:"daniel@mail.com", password:"1234", displayName:"Daniel K.",},
+    {email:"Test", password:"P@sSw0rd", displayName:"DisplayNameTest",}
+  ]);
+  
   return (
     <div className="App">
       <Routes>
-        {/* TODO: index ("/") path should be point to message board, which link back to login form if user not login. */}
+        {/* Notice: if you sent to login from url "localhost:3000/" then it work fine
+                    when user isn't login in the app yet, it send you to login form (see login at Messenger.js)  */}
         <Route path="/" element={<Messenger/>}/>
         <Route path="/login" element={<LoginForm setUser={setUser}/>}/> 
         <Route path="/register" element={<RegisterForm setUser={setUser}/>}/>
--- part-1/web-app/src/Messenger.js
+++ part-1/web-app/src/Messenger.js
@@ -6,6 +6,8 @@ import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function Messenger() {
+
+    // used to send client to different views. ( use navigate('/SOMEWHERE') in logic to jump to localhost:3000/SOMEWHERE )
     const navigate = useNavigate();
     useEffect(() => {    
         validateUser();
@@ -13,11 +15,15 @@ function Messenger() {
 
     function validateUser(){
     // This suppose to check if user is saved in cookie, or state.
-    // For now, as no backend exist, it will check only user state (init as 0)
+    // For now, as no backend exist, it will check only user state (init as null)
+    // Notice: currently it send you back to login form, no matter if you user exist or not.
+    // TODO: should be easy to receive the current user from App component, and check if its legit user (exists in users). 
     console.log(window.location.href);
     
     navigate('/login');
     }
+  // TODO: need to design the Messenger UI. afterword, we need to make the logic works.
+  // Notice: messages information should be kept in App.js for now, when we will have a backend server, we will add it requests to the functions to recive it from there.
   return (
     <Container className="col-12 col-sm-9 col-lg-6  Panel">
     Hello world
