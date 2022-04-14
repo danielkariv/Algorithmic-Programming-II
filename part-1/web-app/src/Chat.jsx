19c6c8ab341867eb6f210c355dce26803f0b4fa0
Daniel Kariv <danielkariv98@gmail.com>
2022-04-14 14:25:40 +0300

    Change layout, and adds basic inputs

--- part-1/web-app/src/Chat.jsx
+++ part-1/web-app/src/Chat.jsx
@@ -1,10 +1,52 @@
+import { Container, Row, Col, Form, Button, Stack} from "react-bootstrap";
 
 
 
 function Chat()
 {
     return (
-       <h1>hello chat</h1> 
+       <Container className="d-flex flex-column" style={{height:"100%"}}>
+            {/* Top bar, image and username with chat with.*/}
+           <Row>
+                <Col>
+                Image
+                </Col>
+                <Col>
+                Second Username
+                </Col>
+           </Row>
+           {/* The chat itself, need to be scrollable, and have rows inside with messages.
+                TODO: make it strech to all avaiable height. */}
+           <Row style={{"flex-grow" : "1"}}>
+                <Row> <p> Test Message</p></Row>
+                <Row> <p> Test Message</p></Row>
+                <Row> <p> Test Message</p></Row>
+                <Row> <p> Test Message</p></Row>
+                <Row> <p> Test Message</p></Row>
+           </Row>
+           {/* Input bar for message, image, video or audio.*/}
+           <Row>
+           <Form>
+                <Row>
+                <Col className='col-2'>
+                    <Button variant="primary" type="submit">
+                    File
+                    </Button>
+                </Col>
+                <Col className='col-8'>
+                    <Form.Group  controlId="formInputMessage">
+                    <Form.Control type="text" placeholder="Enter text ... " />
+                    </Form.Group>
+                </Col>
+                <Col className='col-2'>
+                    <Button variant="primary" type="submit">
+                    Add
+                    </Button>
+                </Col>
+                </Row>
+            </Form>
+           </Row>
+       </Container>
         );
 }
 export default Chat;
--- part-1/web-app/src/Chatbook.jsx
+++ part-1/web-app/src/Chatbook.jsx
@@ -1,7 +1,54 @@
+import { Container,Row,Form,Col,Button,ListGroup,ListGroupItem } from "react-bootstrap";
+
 function Chatbook()
 {
     return (
-       <h1>hello chatbook</h1> 
-        );
+        <Container className="d-flex flex-column" style={{height:"100%"}}>
+       {/* Row of input */}
+       <Row>
+       <Form>
+         <Row>
+           <Col className='col-8'>
+             <Form.Group  controlId="formUsername">
+               <Form.Control type="text" placeholder="username" />
+             </Form.Group>
+           </Col>
+           <Col className='col-4'>
+             <Button variant="primary" type="submit">
+               Add
+             </Button>
+           </Col>
+         </Row>
+       </Form>
+     </Row>
+     {/* Row of list of chat memebers */}
+     <Row style={{"flex-grow" : "1"}}>
+       <ListGroup>
+         {/* Row of a member */}
+         <ListGroupItem>
+           <Row>
+             <Col>
+               Image
+             </Col>
+             <Col>
+               Username
+             </Col>
+           </Row>
+         </ListGroupItem>
+
+         <ListGroupItem>
+           <Row>
+             <Col>
+               Image
+             </Col>
+             <Col>
+               Second Username
+             </Col>
+           </Row>
+         </ListGroupItem>
+       </ListGroup>
+     </Row>
+     </Container>
+    );
 }
 export default Chatbook;
