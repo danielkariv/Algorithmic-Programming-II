ab824b670f297468a7180e131b1f186510fc2197
danielkariv <38776931+danielkariv@users.noreply.github.com>
2022-06-18 15:32:59 +0300

    Added Firebase support for our backend.
    Added Firebase to our backend project.
    Doesn't know if it work yet, need Credential file from firebase.
    But from quick look, it probably work, the code from android side alreadly exist, it should show notification when it receive a message or has new contact.

---
+++ part 3/Messenger-WebAPI/Messenger-WebAPI/Migrations/20220514175645_changes-models.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Messenger_WebAPI.Migrations
+{
+    public partial class changesmodels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiverId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Sent",
+                table: "Messages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Receiver",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sender",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Contacts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Receiver",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Sender",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contacts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiverId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SenderId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Sent",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_SenderId",
+                table: "Messages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_ReceiverId",
+                table: "Messages",
+                column: "ReceiverId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ part 3/Messenger-WebAPI/Messenger-WebAPI/Program.cs
@@ -0,0 +1,102 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Messenger_WebAPI.Data;
+using Messenger_WebAPI.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using Google.Apis.Auth.OAuth2;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<Messenger_WebAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Messenger_WebAPIContext") ?? throw new InvalidOperationException("Connection string 'Messenger_WebAPIContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(45); // 45 mins to timeout.
+    options.Cookie.HttpOnly = false;
+    options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.None;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+});
+builder.Services.AddDistributedMemoryCache();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+
+/*
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()               
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+*/
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder
+              .SetIsOriginAllowed((o) => true)
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+        });
+});
+
+//builder.Services.AddSingleton<IDictionary<string, string>>(opts => new Dictionary<string, string>());
+var app = builder.Build();
+
+//app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAll");
+
+/*
+app.UseCors(builder => builder
+
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()                          
+                            .SetIsOriginAllowed(origin => true)
+                            .AllowCredentials());
+
+*/
+app.UseSession();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseEndpoints(endpoints =>
+ {
+     endpoints.MapHub<Myhub>("/myHub");
+    });
+
+FirebaseAdmin.FirebaseApp.Create(new FirebaseAdmin.AppOptions()
+{
+    Credential = GoogleCredential.FromFile("private_key.json")
+});
+
+app.Run();
+
